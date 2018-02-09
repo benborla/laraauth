f0f686787ee8a3a99214300181a63810d44f1c7c
Ben Borla <benborla@icloud.com>
2018-02-09 09:59:20 -0800

    override login

--- database/migrations/2018_02_09_171506_create_login_verification_table.php
+++ database/migrations/2018_02_09_171506_create_login_verification_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLoginVerificationTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('users')) {
-            Schema::create('users', function (Blueprint $table) {
+        if(!Schema::hasTable('login_verification')) {
+            Schema::create('login_verification', function (Blueprint $table) {
                 $table->increments('id');
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->string('password');
-                $table->rememberToken();
+                $table->integer('user_id')->index();
+                $table->string('security_code');
+                $table->string('device');
                 $table->timestamps();
             });
-            Schema::disableForeignKeyConstraints();
         }
 
     }
@@ -34,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('login_verification');
     }
 }
